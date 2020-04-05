add3dac64144ea0b7803c870eb7f1efbdea50bb4
Matthew Ballance <matt.ballance@gmail.com>
2020-04-05 19:02:40 -0400

    ensure version information is incorporated
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@
 import os
 from setuptools import setup, find_namespace_packages
 
+def get_version():
+    try:
+        import ivpm
+        return ivpm.get_pkg_version(__file__)
+    except:
+        return "0.0.0"
+
+
 setup(
   name = "pyucis",
   packages=find_namespace_packages(where='src'),
   package_dir = {'' : 'src'},
+  version=get_version(),
   author = "Matthew Ballance",
   author_email = "matt.ballance@gmail.com",
-  description = ("pyucis provides a Python API for manipulating UCIS XML files."),
+  description = ("PyUCIS provides a Python API for manipulating UCIS coverage data."),
   license = "Apache 2.0",
   keywords = ["SystemVerilog", "Verilog", "RTL", "Coverage"],
   url = "https://github.com/fvutils/pyucis",
@@ -19,6 +28,7 @@ setup(
   },
   setup_requires=[
     'setuptools_scm',
+    'ivpm'
   ],
   install_requires=[
     'lxml',
